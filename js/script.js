8cae0616c876c56b78b2990864a09757ec62f8e2
gwenabad <abadgwen16@gmail.com>
2023-04-04 19:03:20 +0800

    AboutMe and Experiences Section

--- js/script.js
+++ js/script.js
@@ -4,10 +4,31 @@ console.log("good")
     //additional condi to be done for manual scrolling
 var menuContainer = document.getElementById("menu");
 var menuItems = menuContainer.getElementsByTagName("a");
-for (var i = 0; i < menuItems.length; i++) {
+for (let i = 0; i < menuItems.length; i++) {
   menuItems[i].addEventListener("click", function() {
     var current = document.getElementsByClassName("menu__item--active");
     current[0].classList.remove(...["menu__item--active", "fa-beat", "fa-2xs"])
     this.firstElementChild.classList.add(...["menu__item--active", "fa-beat", "fa-sm"])
   });
+}
+
+//Animation for shaking interest icons
+var interestIconContainer = document.getElementById("interests-container__icons");
+var icons = interestIconContainer.getElementsByTagName("i");
+
+for (let i = 0; i < icons.length; i++) {
+    icons[i].addEventListener("mouseover", function(){
+        var current = document.getElementsByClassName("fa-shake");
+        if(current.length > 0){
+            current[0].classList.remove("fa-shake")
+        }
+        this.classList.add("fa-shake")
+        console.log(this.classList.contains("fa-shake"))
+    })
+}
+
+for (let i = 0; i < icons.length; i++) {
+    icons[i].addEventListener("mouseleave", function(){
+        this.classList.remove("fa-shake")
+    })
 }
